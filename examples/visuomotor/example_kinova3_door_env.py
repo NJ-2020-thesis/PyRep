76b08bd7ccd44e72ab32160febfac2131c834466
njanirudh <anijaya9@gmail.com>
2020-11-05 11:10:39 +0100

    Experimentation

--- examples/visuomotor/example_kinova3_door_env.py
+++ examples/visuomotor/example_kinova3_door_env.py
@@ -14,16 +14,22 @@ from pyrep.errors import ConfigurationPathError
 from pyrep.robots.arms.kinova3 import Kinova3
 from pyrep.objects.camera import Camera
 from pyrep.objects.vision_sensor import VisionSensor 
+
+from utils.dataset_generator import *
+
 import numpy as np
 import math
 import time
+from os import path
 
-EPISODES = 1
+EPISODES = 100
 SCENE_FILE = join(dirname(abspath(__file__)), 'scene_kinova3_door_env.ttt')
 
 pr = PyRep()
 pr.launch(SCENE_FILE, headless=False)
 pr.start()
+
+DATASET_PATH = "/home/anirudh/Desktop/Dataset/"
 
 print("------------LOGIC-------------")
 
@@ -42,16 +48,40 @@ print("BBox : ",handle_box.get_bounding_box())
 
 starting_joint_positions = agent.get_joint_positions()
 
-def move_arm(position, quaternion, orientation ,ignore_collisions=False):
+vision_sensor = VisionSensor("vision_sensor")
+depth_sensor_left =  VisionSensor("depth_sensor_left")
+depth_sensor_right =  VisionSensor("depth_sensor_right")
+
+def move_arm(position, quaternion, orientation , save_path, ignore_collisions=False, save_data=False):
     arm_path = agent.get_path(position=position,
-                            # quaternion=quaternion,
                             euler=[0, math.radians(180), 0],
                             ignore_collisions=ignore_collisions)
     arm_path.visualize()
+    i,max_step = 0,100
     done = False
+
+    vision_data,depth_data_left,depth_data_right = [],[],[]
+    joint_pose_list = []
     while not done:
         done = arm_path.step()
         pr.step()
+        
+        if save_data:
+            vision_data.append(vision_sensor.capture_rgb())
+            depth_data_left.append(depth_sensor_left.capture_rgb())
+            depth_data_right.append(depth_sensor_right.capture_rgb())
+            joint_pose_list.append(agent.get_joint_positions())
+
+            print(len(vision_data),len(depth_data_left),len(vision_data),len(joint_pose_list))
+        
+        i += 1
+
+    if save_data: 
+        np.save(path.join(save_path,"vision.npx"),np.array(vision_data))
+        np.save(path.join(save_path,"depth_data_left.npx"),np.array(depth_data_left))
+        np.save(path.join(save_path,"depth_data_right.npx"),np.array(depth_data_right))
+        np.save(path.join(save_path,"joint_pose_list.npx"),np.array(joint_pose_list))
+
     arm_path.clear_visualization()
 
 
@@ -66,13 +96,12 @@ target = Shape.create(type=PrimitiveShape.SPHERE,
                       color=[1.0, 0.1, 0.1],
                       static=True, respondable=False)
 
-# vision_sensor = VisionSensor("vision_sensor")
-# depth_sensor_left =  VisionSensor("depth_sensor_left")
-# depth_sensor_right =  VisionSensor("depth_sensor_right")
 
 for i in range(EPISODES):
 
     agent.set_joint_positions(starting_joint_positions)
+
+    path_dict = create_folder_structure(DATASET_PATH,i)
 
     # position_min, position_max = [0.8, -0.2, 1.0], [1.0, 0.2, 1.4]
     position_min, position_max = [handle_bounding_box[0],handle_bounding_box[1],handle_bounding_box[2]], \
@@ -81,11 +110,11 @@ for i in range(EPISODES):
     target.set_position(position = list(np.random.uniform(position_min, position_max)))
     target.set_orientation(start_point.get_orientation())
 
-    move_arm(start_point2.get_position(),start_point2.get_quaternion(),start_point2.get_orientation(),True)
-    move_arm(start_point1.get_position(),start_point1.get_quaternion(),start_point1.get_orientation(),True)
-    move_arm(start_point0.get_position(),start_point0.get_quaternion(),start_point0.get_orientation(),True)
-    move_arm(start_point.get_position(),start_point.get_quaternion(),start_point.get_orientation(),True)
-    # move_arm(target.get_position(),target.get_quaternion(),target.get_orientation(),True)
+    move_arm(start_point2.get_position(),start_point2.get_quaternion(),start_point2.get_orientation(),path_dict["episode_path"],True,False)
+    move_arm(start_point1.get_position(),start_point1.get_quaternion(),start_point1.get_orientation(),path_dict["episode_path"],True,False)
+    move_arm(start_point0.get_position(),start_point0.get_quaternion(),start_point0.get_orientation(),path_dict["episode_path"],True,False)
+    move_arm(start_point.get_position(),start_point.get_quaternion(),start_point.get_orientation(),path_dict["episode_path"],True,False)
+    move_arm(target.get_position(),target.get_quaternion(),target.get_orientation(),path_dict["episode_path"],True,False)
 
 print("--------------------------------")
 
--- examples/visuomotor/utils/dataset_generator.py
+++ examples/visuomotor/utils/dataset_generator.py
@@ -1,38 +1,31 @@
-import numpy
+import numpy as np
 from pathlib import Path
 
 def create_folder_structure(dataset_path: str, episode: int) -> dict:
     episode_path = Path(dataset_path).joinpath("episode_" + str(episode))
     create_folder(episode_path)
 
-    # --------------------------------------------------
-    joint_position_path = episode_path.joinpath("joint_position")
-    create_folder(joint_position_path)
+    # # --------------------------------------------------
 
-    robot_pose_path = episode_path.joinpath("robot_pose")
-    create_folder(robot_pose_path)
+    # robot_image_path = episode_path.joinpath("image")
+    # create_folder(robot_image_path)
 
-    robot_image_path = episode_path.joinpath("image")
-    create_folder(robot_image_path)
+    # # --------------------------------------------------
+    # left_image_path = robot_image_path.joinpath("left")
+    # create_folder(left_image_path)
 
-    # --------------------------------------------------
-    left_image_path = robot_image_path.joinpath("left")
-    create_folder(left_image_path)
+    # right_image_path = robot_image_path.joinpath("right")
+    # create_folder(right_image_path)
 
-    right_image_path = robot_image_path.joinpath("right")
-    create_folder(right_image_path)
-
-    depth_image_path = robot_image_path.joinpath("depth")
-    create_folder(depth_image_path)
+    # depth_image_path = robot_image_path.joinpath("depth")
+    # create_folder(depth_image_path)
 
     print("Saved data for < Episode : ", str(episode), " >")
-    path_dict = {"episode_path": episode_path,
-                 "joint_position_path": joint_position_path,
-                 "robot_pose_path": robot_pose_path,
-                 "robot_image_path": robot_image_path,
-                 "left_image_path": left_image_path,
-                 "right_image_path": right_image_path,
-                 "depth_image_path": depth_image_path}
+    path_dict = {"episode_path": episode_path}
+                #  "robot_image_path": robot_image_path,
+                #  "left_image_path": left_image_path,
+                #  "right_image_path": right_image_path,
+                #  "depth_image_path": depth_image_path}
 
     return path_dict
 
