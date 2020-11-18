cc89683df7b403b72cafe6b939a799aa6586f96f
njanirudh <anijaya9@gmail.com>
2020-11-18 13:13:56 +0100

    Random env working

--- examples/visuomotor/example_kinova3_door_env_random.py
+++ examples/visuomotor/example_kinova3_door_env_random.py
@@ -10,6 +10,7 @@ from pyrep.objects.camera import Camera
 from pyrep.objects.vision_sensor import VisionSensor 
 
 from utils.dataset_generator import *
+from utils.manip_utils import move_arm
 
 import numpy as np
 import math
@@ -18,7 +19,7 @@ import random
 from os import path
 
 
-EPISODES = 26
+EPISODES = 2
 SCENE_FILE = join(dirname(abspath(__file__)), 'scene_kinova3_door_env_random.ttt')
 
 pr = PyRep()
@@ -35,47 +36,33 @@ starting_joint_positions = agent.get_joint_positions()
 agent.set_joint_positions(starting_joint_positions)
 
 gripper = Shape('ROBOTIQ_85')
-initial_gripper_positions = gripper.get_configuration_tree()
+gripper_state = gripper.get_configuration_tree()
 
 handle = Shape("door_handle_visible")
 handle_bounding_box = handle.get_bounding_box()
 
+door = Shape('door_frame')
+door_state = door.get_configuration_tree()
 
-def move_arm(position, quaternion, orientation, ignore_collisions=False):
-    arm_path = agent.get_linear_path(position=position,
-                            euler=[0, math.radians(180), 0],
-                            # euler=[0, 0, 0],
-                            ignore_collisions=ignore_collisions)
-    arm_path.visualize()
-    done = False
-
-    while not done:
-        done = arm_path.step()
-        pr.step()
-
-    arm_path.clear_visualization()
-
-
-
-start_point = Dummy('start_point')
-start_point0 = Dummy('start_point0')
-start_point1 = Dummy('start_point1')
-start_point2 = Dummy('start_point2')
+# ---------------------------------------
 
 target = Dummy('start_point3')
 
 position_min, position_max = [handle_bounding_box[0],handle_bounding_box[2],handle_bounding_box[4]], \
-                            [handle_bounding_box[1],handle_bounding_box[3],handle_bounding_box[5]*0.1]
-random_pose = list(np.arange(0.01,0.05,0.001))
+                            [handle_bounding_box[1],handle_bounding_box[3],handle_bounding_box[5]*0.01]
+random_pose = list(np.arange(-0.05,0.05,0.002))
 
 for i in range(EPISODES):
 
     eps = random.sample(random_pose, 7)
+    eps[6] = random.sample(list(np.arange(-3.0,3.0,0.4)),1)[0]
     agent.set_joint_positions(np.add(starting_joint_positions,eps))
+    # agent.set_joint_positions([0,0,0,0,0,0,0])
+    # print(agent.get_joint_intervals())
 
-    pr.set_configuration_tree(initial_gripper_positions)
+    pr.set_configuration_tree(gripper_state)
+    pr.set_configuration_tree(door_state)
 
-        # print(position_min,position_max)
     target.set_position(position = list(np.random.uniform(position_min, position_max)),relative_to=handle)
         # target.set_position([0,0,0],relative_to=handle)
 
@@ -84,8 +71,8 @@ for i in range(EPISODES):
     # move_arm(start_point1.get_position(),start_point1.get_quaternion(),start_point1.get_orientation(),False)
     # move_arm(start_point0.get_position(),start_point0.get_quaternion(),start_point0.get_orientation(),False)
     # move_arm(start_point.get_position(),start_point.get_quaternion(),start_point.get_orientation(),False)
-        move_arm(target.get_position(),target.get_quaternion(),target.get_orientation(),True)
-        print("SUccessful!")
+        move_arm(pr,agent,target.get_position(),target.get_quaternion(),target.get_orientation(),True)
+        print("Successful!")
     except:
         print("SKIPPING")
 
---
+++ examples/visuomotor/utils/manip_utils.py
@@ -0,0 +1,21 @@
+import math
+from pyrep import PyRep
+from pyrep.errors import ConfigurationPathError
+
+
+def move_arm(pr,agent,position, quaternion, orientation, ignore_collisions=False):
+    arm_path = agent.get_path(position=position,
+                            # euler=[0, math.radians(180), 0],
+                            # orientation=orientation,
+                            euler=[0, 0, 0],
+                            ignore_collisions=ignore_collisions,
+                            )
+    arm_path.visualize()
+    done = False
+
+    while not done:
+        done = arm_path.step()
+        # print(agent.get_joint_positions())
+        pr.step()
+
+    arm_path.clear_visualization()
