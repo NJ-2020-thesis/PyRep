101c4b3d5671a4da4d79b6b3a23000b934e26e44
njanirudh <anijaya9@gmail.com>
2021-02-09 23:31:28 +0100

    Testing vec env (Not working)

--- examples/visuomotor/baseline_door_env_training.py
+++ examples/visuomotor/baseline_door_env_training.py
@@ -1,40 +1,61 @@
-import gym
-from gym import spaces
+# https://github.com/araffin/rl-tutorial-jnrr19/blob/sb3/1_getting_started.ipynb
+# https://stable-baselines3.readthedocs.io/en/master/guide/callbacks.html
+# https://stable-baselines3.readthedocs.io/en/master/guide/rl_tips.html?highlight=output
+# http://amid.fish/reproducing-deep-rl?fbclid=IwAR1VPZm3FSTrV8BZ4UdFc2ExZy0olusmaewmloTPhpA4QOnHKRI2LLOz3mM
+# https://medium.com/aureliantactics/understanding-ppo-plots-in-tensorboard-cbc3199b9ba2
+# https://stable-baselines.readthedocs.io/en/master/guide/examples.html
 
-from stable_baselines3 import PPO
-from stable_baselines3.common.env_checker import check_env
-
-from pyrep.objects.vision_sensor import VisionSensor 
-
-from os.path import dirname, join, abspath
-from pyrep import PyRep
-from pyrep.robots.arms.panda import Panda
-from pyrep.robots.arms.kinova3 import Kinova3
-from pyrep.objects.shape import Shape
-from pyrep.objects.dummy import Dummy
-import numpy as np
 import pickle
+import uuid
+from os.path import abspath, dirname, join
+from scipy.interpolate import interp1d
 
-rewards_file = "/home/anirudh/HBRS/Master-Thesis/NJ-2020-thesis/PyRep/examples/visuomotor/results/rewards_1000.txt"
-model_path = "/home/anirudh/HBRS/Master-Thesis/NJ-2020-thesis/PyRep/examples/visuomotor/results/model_saved_1000.mdl"
+import gym
+import numpy as np
+from gym import spaces
+from pyrep import PyRep
+from pyrep.objects.dummy import Dummy
+from pyrep.objects.shape import Shape
+from pyrep.objects.vision_sensor import VisionSensor
+from pyrep.robots.arms.kinova3 import Kinova3
+from pyrep.robots.arms.panda import Panda
+import stable_baselines3
+from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import CheckpointCallback
+from stable_baselines3.common.env_checker import check_env
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize, SubprocVecEnv
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.utils import set_random_seed
+
+# Paths to save values
+rewards_file = "/home/anirudh/HBRS/Master-Thesis/NJ-2020-thesis/PyRep/examples/visuomotor/results/rewards"+ str(uuid.uuid4())[:8] +"_1000.txt"
+model_path = "/home/anirudh/HBRS/Master-Thesis/NJ-2020-thesis/PyRep/examples/visuomotor/results/model_"+ str(uuid.uuid4())[:8] +"_1000.mdl"
 log_path = "/home/anirudh/HBRS/Master-Thesis/NJ-2020-thesis/PyRep/examples/visuomotor/log/"
 
 SCENE_FILE = join(dirname(abspath(__file__)),
                   'scene_kinova3_door_env_random.ttt')
 
+# Hyperparams
 POLICY_UPDATE_STEPS = 50
-EPISODE_LENGTH = 300
-TOTAL_TIMESTEPS = 1000
+EPISODE_LENGTH = 200
+TOTAL_TIMESTEPS = 100000
+
+# GPU
+print("GPU Available : ",stable_baselines3.common.utils.get_device())
 
 class ReacherEnv(gym.Env):
 
     def __init__(self):
         super(ReacherEnv,self).__init__()
 
+        # Creating a Custom Pyrep environment 
         self.pr = PyRep()
         self.pr.launch(SCENE_FILE, headless=False)
         self.pr.start()
 
+        # SavinCreating the Robot and Other objects
+        # These will be used for collision, action space etc.
         self.agent = Kinova3()
         self.agent.set_control_loop_enabled(False)
         self.agent.set_motor_locked_at_zero_velocity(True)
@@ -49,7 +70,7 @@ class ReacherEnv(gym.Env):
         self.handle = Shape("door_handle_visible")
         handle_bounding_box = self.handle.get_bounding_box()
 
-        # Env initial state
+        # Saving Env initial state
         self.gripper = Shape('ROBOTIQ_85')
         self.gripper_state = self.gripper.get_configuration_tree()
         self.door = Shape('door_frame')
@@ -65,47 +86,67 @@ class ReacherEnv(gym.Env):
         self.target.set_position(position = self.random_handle_pos,relative_to=self.handle)
 
         # Setting action and state space for robot 
-        # self.action_space = spaces.Box(low=np.asarray([val[0] for val in self.agent.get_joint_intervals()[1]]),
-        #                              high=np.asarray([val[1] for val in self.agent.get_joint_intervals()[1]]), dtype=np.float)
-        self.action_space = spaces.Box(low=np.asarray([-val for val in self.agent.get_joint_upper_velocity_limits()]),
-                                     high=np.asarray([val for val in self.agent.get_joint_upper_velocity_limits()]), dtype=np.float)
-        self.observation_space = spaces.Box(low=-3.0, high=3.0, shape=(7,), dtype=np.float)
+        self.observation_space = spaces.Box(low=np.asarray([val[0] for val in self.agent.get_joint_intervals()[1]]),
+                                     high=np.asarray([val[1] for val in self.agent.get_joint_intervals()[1]]), dtype=np.float)
+
+        # Normalize between [-1,1]
+        self.action_space = spaces.Box(low=-1, high=1, shape=(7,), dtype=np.float32)
+        # self.action_space = spaces.Box(low=np.asarray([-val[0] for val in self.agent.get_joint_intervals()[1]]),
+        #                              high=np.asarray([val[0] for val in self.agent.get_joint_intervals()[1]]), dtype="float32")
+        # self.action_space = spaces.Box(low=np.asarray([-val for val in self.agent.get_joint_upper_velocity_limits()]),
+        #                              high=np.asarray([val for val in self.agent.get_joint_upper_velocity_limits()]), dtype=np.float)
 
 
     def _get_state(self):
         # Return state containing arm joint angles/velocities & target position
+        # initial_image = self.vision_sensor.capture_rgb()
         # print(self.agent.get_joint_intervals())
-        # print(self.agent.get_joint_upper_velocity_limits())
-        initial_image = self.vision_sensor.capture_rgb()
         # initial_representation = 
         # goal_representation = 
         return np.concatenate([self.agent.get_joint_positions()])
 
     def reset(self):
-        # Get a random position within a cuboid and set the target position
-        # Reset env
-        self.agent.set_joint_positions(self.initial_joint_positions)
+        # Reset environment with initial conditions
+        self.agent.set_joint_target_positions(self.initial_joint_positions)
         self.pr.set_configuration_tree(self.gripper_state)
         self.pr.set_configuration_tree(self.door_state)
         self.target.set_position(position = self.random_handle_pos,relative_to=self.handle)
         return self._get_state()
 
     def step(self, action):
-        if self.dining_table.check_collision() == True :
-            self.reset()
+        done = False
 
-        self.agent.set_joint_target_velocities(action)
-        print(self.dining_table.check_collision())
+        # Denorm values
+        lower_limits = [val[0] for val in self.agent.get_joint_intervals()[1]]
+        upper_limits = [val[1] for val in self.agent.get_joint_intervals()[1]]
+        denorm_action = [] 
+
+        for num,low,high in zip(action,lower_limits,upper_limits):
+            new = np.interp(num,[-1,1],[low,high])
+            denorm_action.append(new)
+
+        # print(lower_limits)
+        # print(upper_limits)
+        # print(action)
+        # print(denorm_action)
+        # print("")
+        # self.agent.set_joint_target_velocities(action)
+        self.agent.set_joint_target_positions(denorm_action)
 
         self.pr.step()  # Step the physics simulation
+
+        # Reward is negative distance to target
         ax, ay, az = self.agent_ee_tip.get_position()
         tx, ty, tz = self.target.get_position()
 
-        # Reward is negative distance to target
         reward = -np.sqrt((ax - tx) ** 2 + (ay - ty) ** 2 + (az - tz) ** 2)
         info = {}
-        done = False
-        # print(reward)
+
+        # Simple collision detection
+        if self.dining_table.check_collision() == True :
+            print(self.dining_table.check_collision())
+            done = True
+            return self._get_state(),reward,done,info
 
         return self._get_state(),reward,done,info
 
@@ -113,29 +154,58 @@ class ReacherEnv(gym.Env):
         self.pr.stop()
         self.pr.shutdown()
 
-load_model_path = "/home/anirudh/HBRS/Master-Thesis/NJ-2020-thesis/PyRep/examples/visuomotor/results/ppo_saved_test.mdl"
+# ------------------------------------------------------------------
+def make_env(env_id, rank, seed=666):
+    """
+    Utility function for multiprocessed env.
+
+    :param env_id: (str) the environment ID
+    :param num_env: (int) the number of environments you wish to have in subprocesses
+    :param seed: (int) the inital seed for RNG
+    :param rank: (int) index of the subprocess
+    """
+    def _init():
+        env = ReacherEnv()
+        env.seed(seed + rank)
+        return env
+    set_random_seed(seed)
+    return _init
+
+
+checkpoint_callback = CheckpointCallback(save_freq=10000, save_path='./logs/',
+                                         name_prefix='rl_model_norm_gpu')
+
+# env = SubprocVecEnv([make_vec_env("abc", i) for i in range(4)])
 env = ReacherEnv()
-model = PPO('MlpPolicy',n_steps = EPISODE_LENGTH, n_epochs= POLICY_UPDATE_STEPS ,env=env, verbose=2,tensorboard_log=log_path)
-# model.learn(total_timesteps=TOTAL_TIMESTEPS)
+env.seed(666)
+model = PPO('MlpPolicy',n_steps = EPISODE_LENGTH, n_epochs= POLICY_UPDATE_STEPS ,
+                env=env, verbose=2, tensorboard_log=log_path)
+
+print(env.spec)
+
+# +++++ Training +++++
+
+# model.learn(total_timesteps=TOTAL_TIMESTEPS, callback=checkpoint_callback)
 # model.save(model_path)
 
-model.load(load_model_path,env)
+# +++++ Evaluation +++++
+# model.load("/home/anirudh/HBRS/Master-Thesis/NJ-2020-thesis/PyRep/examples/visuomotor/logs/rl_model_400000_steps.zip")
+# mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=5, return_episode_rewards=True)
+# print("====>","mean_reward : ",mean_reward ,"std_reward : ",std_reward)
 
-# check_env(env, warn=True)
 
+# +++++ Random Eval +++++
 
-# rewards = []
+# env = ReacherEnv()
 # obs = env.reset()
-# for i in range(EPISODE_LENGTH):
-
-#     action, _states = model.predict(obs, deterministic=True)
-#     obs,reward,done,info = env.step(action)
-#     rewards.append(reward)
-#     if done:
-#       obs = env.reset()
-#     print("ENV : ",i," ", "REWARD : ",reward)
-
-# np.save(rewards_file,rewards,allow_pickle=False)
+# n_steps = 10000
+# for i in range(n_steps):
+#     # Random action
+#     action = env.action_space.sample()
+#     # print(action)
+#     obs, reward, done, info = env.step(action)
+#     if done or (i%3000 == 0):
+#         obs = env.reset()
 
 env.close()
 env.shutdown()
