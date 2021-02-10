77bb1b81031f73c2dc5f49bb4f838d7fa78e637f
njanirudh <anijaya9@gmail.com>
2021-02-10 14:25:44 +0100

    Added new rewards

--- examples/visuomotor/baseline_door_env_training.py
+++ examples/visuomotor/baseline_door_env_training.py
@@ -4,6 +4,7 @@
 # http://amid.fish/reproducing-deep-rl?fbclid=IwAR1VPZm3FSTrV8BZ4UdFc2ExZy0olusmaewmloTPhpA4QOnHKRI2LLOz3mM
 # https://medium.com/aureliantactics/understanding-ppo-plots-in-tensorboard-cbc3199b9ba2
 # https://stable-baselines.readthedocs.io/en/master/guide/examples.html
+# https://medium.com/pytorch/robotic-assembly-using-deep-reinforcement-learning-dfd9916c5ad7
 
 import pickle
 import uuid
@@ -19,6 +20,8 @@ from pyrep.objects.shape import Shape
 from pyrep.objects.vision_sensor import VisionSensor
 from pyrep.robots.arms.kinova3 import Kinova3
 from pyrep.robots.arms.panda import Panda
+from pyrep.objects.proximity_sensor import ProximitySensor 
+
 import stable_baselines3
 from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import CheckpointCallback
@@ -37,7 +40,7 @@ SCENE_FILE = join(dirname(abspath(__file__)),
                   'scene_kinova3_door_env_random.ttt')
 
 # Hyperparams
-POLICY_UPDATE_STEPS = 50
+POLICY_UPDATE_STEPS = 50 
 EPISODE_LENGTH = 200
 TOTAL_TIMESTEPS = 100000
 
@@ -70,12 +73,13 @@ class ReacherEnv(gym.Env):
         self.handle = Shape("door_handle_visible")
         handle_bounding_box = self.handle.get_bounding_box()
 
+        self.proximity_sensor = ProximitySensor('ROBOTIQ_85_attachProxSensor')
+
         # Saving Env initial state
         self.gripper = Shape('ROBOTIQ_85')
         self.gripper_state = self.gripper.get_configuration_tree()
         self.door = Shape('door_frame')
         self.door_state = self.door.get_configuration_tree()
-
 
         # Bounding box within wich we get successful grasp
         BOUNDING_BOX_EPS = 0.003
@@ -89,21 +93,30 @@ class ReacherEnv(gym.Env):
         self.observation_space = spaces.Box(low=np.asarray([val[0] for val in self.agent.get_joint_intervals()[1]]),
                                      high=np.asarray([val[1] for val in self.agent.get_joint_intervals()[1]]), dtype=np.float)
 
+        self.initial_distance = self.distance_to_goal()
+        # print("Initial distance : ",self.initial_distance)
         # Normalize between [-1,1]
-        self.action_space = spaces.Box(low=-1, high=1, shape=(7,), dtype=np.float32)
+        self.action_space = spaces.Box(low=-1., high=1., shape=(7,), dtype=np.float32)
         # self.action_space = spaces.Box(low=np.asarray([-val[0] for val in self.agent.get_joint_intervals()[1]]),
         #                              high=np.asarray([val[0] for val in self.agent.get_joint_intervals()[1]]), dtype="float32")
         # self.action_space = spaces.Box(low=np.asarray([-val for val in self.agent.get_joint_upper_velocity_limits()]),
         #                              high=np.asarray([val for val in self.agent.get_joint_upper_velocity_limits()]), dtype=np.float)
 
+    def distance_to_goal(self):
+        # Reward is negative distance to target
+        ax, ay, az = self.agent_ee_tip.get_position()
+        tx, ty, tz = self.target.get_position()
+
+        reward = -np.sqrt((ax - tx) ** 2 + (ay - ty) ** 2 + (az - tz) ** 2)
+        return reward
 
     def _get_state(self):
         # Return state containing arm joint angles/velocities & target position
         # initial_image = self.vision_sensor.capture_rgb()
-        # print(self.agent.get_joint_intervals())
         # initial_representation = 
         # goal_representation = 
-        return np.concatenate([self.agent.get_joint_positions()])
+        joint_pos = self.agent.get_joint_positions()
+        return np.concatenate([joint_pos])
 
     def reset(self):
         # Reset environment with initial conditions
@@ -111,10 +124,14 @@ class ReacherEnv(gym.Env):
         self.pr.set_configuration_tree(self.gripper_state)
         self.pr.set_configuration_tree(self.door_state)
         self.target.set_position(position = self.random_handle_pos,relative_to=self.handle)
+        self.initial_distance = self.distance_to_goal()
+
         return self._get_state()
 
     def step(self, action):
         done = False
+        info = {}
+        prev_distance_to_goal = self.distance_to_goal()
 
         # Denorm values
         lower_limits = [val[0] for val in self.agent.get_joint_intervals()[1]]
@@ -125,57 +142,40 @@ class ReacherEnv(gym.Env):
             new = np.interp(num,[-1,1],[low,high])
             denorm_action.append(new)
 
-        # print(lower_limits)
-        # print(upper_limits)
-        # print(action)
-        # print(denorm_action)
-        # print("")
         # self.agent.set_joint_target_velocities(action)
         self.agent.set_joint_target_positions(denorm_action)
-
         self.pr.step()  # Step the physics simulation
 
-        # Reward is negative distance to target
-        ax, ay, az = self.agent_ee_tip.get_position()
-        tx, ty, tz = self.target.get_position()
-
-        reward = -np.sqrt((ax - tx) ** 2 + (ay - ty) ** 2 + (az - tz) ** 2)
-        info = {}
+        # Reward calculations
+        success_reward = self.success_check()
+        distance_reward = (prev_distance_to_goal - self.distance_to_goal()) / self.initial_distance
+        reward = distance_reward + success_reward
 
         # Simple collision detection
         if self.dining_table.check_collision() == True :
-            print(self.dining_table.check_collision())
             done = True
             return self._get_state(),reward,done,info
-
+        elif self.distance_to_goal() > 0.8:
+            done = True
+        
+        print(reward)
         return self._get_state(),reward,done,info
+
+    def success_check(self):
+        success_reward = 0.
+        if self.distance_to_goal() < 0.01:
+            success_reward = 10
+        return success_reward
 
     def shutdown(self):
         self.pr.stop()
         self.pr.shutdown()
 
-# ------------------------------------------------------------------
-def make_env(env_id, rank, seed=666):
-    """
-    Utility function for multiprocessed env.
+## ------------------------------------------------------------------
 
-    :param env_id: (str) the environment ID
-    :param num_env: (int) the number of environments you wish to have in subprocesses
-    :param seed: (int) the inital seed for RNG
-    :param rank: (int) index of the subprocess
-    """
-    def _init():
-        env = ReacherEnv()
-        env.seed(seed + rank)
-        return env
-    set_random_seed(seed)
-    return _init
+checkpoint_callback = CheckpointCallback(save_freq=10000, save_path='./models/1/',
+                                         name_prefix='norm_gpu_new')
 
-
-checkpoint_callback = CheckpointCallback(save_freq=10000, save_path='./logs/',
-                                         name_prefix='rl_model_norm_gpu')
-
-# env = SubprocVecEnv([make_vec_env("abc", i) for i in range(4)])
 env = ReacherEnv()
 env.seed(666)
 model = PPO('MlpPolicy',n_steps = EPISODE_LENGTH, n_epochs= POLICY_UPDATE_STEPS ,
@@ -183,18 +183,17 @@ model = PPO('MlpPolicy',n_steps = EPISODE_LENGTH, n_epochs= POLICY_UPDATE_STEPS 
 
 print(env.spec)
 
-# +++++ Training +++++
+## +++++ Training +++++
+model.learn(total_timesteps=TOTAL_TIMESTEPS, callback=checkpoint_callback)
+model.save(model_path)
 
-# model.learn(total_timesteps=TOTAL_TIMESTEPS, callback=checkpoint_callback)
-# model.save(model_path)
-
-# +++++ Evaluation +++++
-# model.load("/home/anirudh/HBRS/Master-Thesis/NJ-2020-thesis/PyRep/examples/visuomotor/logs/rl_model_400000_steps.zip")
+# # +++++ Evaluation +++++
+# model.load("/home/anirudh/HBRS/Master-Thesis/NJ-2020-thesis/PyRep/examples/visuomotor/models/norm_gpu_new_800000_steps.zip")
 # mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=5, return_episode_rewards=True)
 # print("====>","mean_reward : ",mean_reward ,"std_reward : ",std_reward)
 
 
-# +++++ Random Eval +++++
+## +++++ Random Eval +++++
 
 # env = ReacherEnv()
 # obs = env.reset()
